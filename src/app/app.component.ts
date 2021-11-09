d96933c6290c24d7489c6d4b7a94d50508946e0c
arsiasieg <cam.lucas89@gmail.com>
2021-11-09 15:15:05 +0100

    Delete useless code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NasaService } from './nasa.service';
-import { DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,23 +8,22 @@ import { DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 })
 export class AppComponent implements OnInit {
   
-  public imgOfTheDay: string | SafeResourceUrl |undefined
+  public imgOfTheDay: string |undefined
   public imgTitle: string | undefined
   public imgUrl: string | undefined
   public imgDate: string | undefined
   public imgExplanation: string | undefined
 
-  constructor(private nasaService: NasaService, private sanitizer: DomSanitizer){
+  constructor(private nasaService: NasaService){
   }
 
   ngOnInit(): void {
-    this.nasaService.getImageOfTheDay().subscribe((image: any) => {
-      // this.imgOfTheDay = this.sanitizer.bypassSecurityTrustHtml(image.url);
-      this.imgOfTheDay =image.url;
-      this.imgTitle = image.title;
-      this.imgUrl = image.url;
-      this.imgDate = image.date;
-      this.imgExplanation = image.explanation;
+    this.nasaService.getImageOfTheDay().subscribe((apod: any) => {
+      this.imgOfTheDay = apod.url;
+      this.imgTitle = apod.title;
+      this.imgUrl = apod.url;
+      this.imgDate = apod.date;
+      this.imgExplanation = apod.explanation;
     })
   }
 }
